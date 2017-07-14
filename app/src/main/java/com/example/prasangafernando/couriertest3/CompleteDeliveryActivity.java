6b625726e6505402f68940dc01c30c594dca26f3
prasangafdz@outlook.com <LykanHypersp0rt@#>
2017-07-13 18:18:09 -0800

    Added to Branch 'GAMEMAX'

---
+++ app/src/main/java/com/example/prasangafernando/couriertest3/CompleteDeliveryActivity.java
@@ -0,0 +1,65 @@
+package com.example.prasangafernando.couriertest3;
+
+import android.*;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+
+import com.google.android.gms.vision.barcode.Barcode;
+
+public class CompleteDeliveryActivity extends AppCompatActivity {
+
+    Button scanbtn;
+    TextView tvParcelID;
+    public static final int REQUEST_CODE = 100;
+    public static final int PERMISSION_REQUEST = 200;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complete_delivery);
+
+        scanbtn = (Button) findViewById(R.id.btnScanCode);
+        tvParcelID = (TextView) findViewById(R.id.tvParcelID);
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, PERMISSION_REQUEST);
+        }
+        scanbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CompleteDeliveryActivity.this, ScanActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK){
+            if(data != null){
+                final Barcode barcode = data.getParcelableExtra("barcode");
+                tvParcelID.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        tvParcelID.setText(barcode.displayValue);
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/prasangafernando/couriertest3/SwitchVehiclesActivity.java
@@ -0,0 +1,14 @@
+package com.example.prasangafernando.couriertest3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SwitchVehiclesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_switch_vehicles);
+
+    }
+}
